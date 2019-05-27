799c6c748a60b8c07040305ab042d2b04e73788d
randycde <42583901+randycde@users.noreply.github.com>
2019-05-27 14:17:58 -0500

    Add happy path tests

--- src/test/java/com/example/RestTemplateTest/RestTemplateTest.java
+++ src/test/java/com/example/RestTemplateTest/RestTemplateTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -25,11 +26,48 @@ public class RestTemplateTest {
         @Resource(name = "defaultRestTemplate")
         RestTemplate defaultRestTemplate;
 
+        @Test
+        @LoadBalanced
+        public void testDefaultRestTemplate0 () {
+
+            ResponseEntity<String> responseEntity;
+            try {
+                HttpEntity<String> httpEntity = new HttpEntity<>("{ }");
+                responseEntity = defaultRestTemplate.exchange("http://localhost:8080/test/0", HttpMethod.POST, httpEntity, String.class);
+            } catch (HttpClientErrorException e) {
+                System.out.println(e.getRawStatusCode());
+                System.out.println(e.getResponseBodyAsString());
+                Assert.fail("Unexpected exception!");
+                return;
+            }
+            Assert.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+            Assert.assertEquals("{ \"result\" = \"OK\" }", responseEntity.getBody().toString());
+
+        }
 
 
         @Test
         @LoadBalanced
-        public void testRestTemplate1 () {
+        public void testApacheRestTemplate0 () {
+
+            ResponseEntity<String> responseEntity;
+            try {
+                HttpEntity<String> httpEntity = new HttpEntity<>("{ }");
+                responseEntity = apacheRestTemplate.exchange("http://localhost:8080/test/0", HttpMethod.POST, httpEntity, String.class);
+            } catch (HttpClientErrorException e) {
+                System.out.println(e.getRawStatusCode());
+                System.out.println(e.getResponseBodyAsString());
+                Assert.fail("Unexpected exception!");
+                return;
+            }
+            Assert.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+            Assert.assertEquals("{ \"result\" = \"OK\" }", responseEntity.getBody().toString());
+
+        }
+
+        @Test
+        @LoadBalanced
+        public void testDefaultRestTemplate1 () {
 
             try {
                 HttpEntity<String> httpEntity = new HttpEntity<>("{ }");
@@ -45,9 +83,10 @@ public class RestTemplateTest {
             Assert.fail("should not get here");
 
         }
+
         @Test
         @LoadBalanced
-        public void testRestTemplate2 () {
+        public void testApacheRestTemplate1 () {
 
             try {
                 HttpEntity<String> httpEntity = new HttpEntity<>("{ }");
